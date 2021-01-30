4e541386c4329122304cdc97e3a2815fdbe72566
Eric Gordon <eric.gordon@ucdconnect.ie>
2021-01-30 23:53:29 +0000

    Added definitionToList function, properly deals with punctuation and example sentences Use separate list to keep track of seen but not yet expanded words

--- src/Main.java
+++ src/Main.java
@@ -7,62 +7,85 @@ import edu.mit.jwi.item.POS;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     private static IDictionary dict;
+    private static Map<String, IWord> map;
+
 
     public static void main(String[] args) throws IOException {
 
         dict = new Dictionary(new File("C:\\Program Files (x86)\\WordNet\\2.1\\dict"));
         dict.open();
 
-        Map<String, IWord> map = new HashMap<>();
+        map = new HashMap<>();
 
-        String current = "apple";
         String gloss;
-        for (int i = 0; i < 10; i++) {
-            List<IIndexWord> idxWords = expandDefinition(current);
-            //System.out.println(idxWords.size() + " ---");
-            for (IIndexWord idxWord : idxWords) {
-
-                IWordID wordID = idxWord.getWordIDs().get(0);
-                IWord word = dict.getWord(wordID);
-                //System.out.println("Id = " + wordID);
-                System.out.println("-" + word.getLemma());
-                gloss = word.getSynset().getGloss();
-                //System.out.println("Gloss = " + gloss);
-                for (String s : gloss.split(" ")) {
-                    map.putIfAbsent(s, word);
-                }
-                current = gloss;
+        LinkedList<IIndexWord> wordList = new LinkedList<>();
+        for (POS pos : POS.values()) {
+            IIndexWord idxWord = dict.getIndexWord("apple", pos);
+            if (idxWord != null ) {
+                wordList.add(idxWord);
             }
-            System.out.println("Iter " + i + ": " + map.size() + " total senses");
         }
+
+        //for (int i = 0; i < 10; i++) {
+
+            //System.out.println(idxWords.size() + " ---");
+            //for (IIndexWord idxWord : idxWords) {
+        int i = 0;
+        while (!wordList.isEmpty() /*&& i < 300*/){
+            IIndexWord idxWord = wordList.remove();
+            IWordID wordID = idxWord
+                    .getWordIDs()
+                    .get(0);
+            IWord word = dict.getWord(wordID);
+            //System.out.println("Id = " + wordID);
+            System.out.println("-" + word.getLemma());
+            gloss = word.getSynset().getGloss();
+            //System.out.println("Gloss = " + gloss);
+            wordList.addAll(expandDefinitionOfWord(word));
+            for (String s : definitionToList(gloss)) {
+                map.putIfAbsent(s, word);
+            }
+            //}
+            System.out.println("Iter " + i++ + ": \n\tMap size = " + map.size() + "\n\tList size = " + wordList.size());
+        }
+        System.out.println("End: " + map.size() + " total senses");
+    }
+    private static List<String> definitionToList(String s){
+        String punctuation = "[!._,'@?; ]";
+        String glossOnly = s.split("\"")[0];
+        //excludes example sentences
+        StringTokenizer tokenizer = new StringTokenizer(glossOnly, punctuation);
+        List<String> result = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()){
+            result.add(tokenizer.nextToken());
+        }
+        //System.out.println(result);
+        return result;
     }
 
-    private static List<IIndexWord> expandDefinition(String gloss) {
+    private static List<IIndexWord> expandDefinitionOfWord(IWord word) {
 
-        List<IIndexWord> validWords = new ArrayList<>();
+        List<IIndexWord> validUnseenWords = new ArrayList<>();
         int count = 0;
-        for (String s : gloss.split(" ")) {
-            //TODO: Refine punctuation parsing, and remove example sentences
-            for (POS pos : POS.values()) {
-                IIndexWord idxWord = dict.getIndexWord(s, pos);
-                if (idxWord != null && !idxWord.getLemma().equals("a")) {
-                    validWords.add(idxWord);
+        for (String s : definitionToList(word.getSynset().getGloss())) {
+            //for (POS pos : POS.values()) {
+                IIndexWord idxWord = dict.getIndexWord(s, POS.ADJECTIVE);
+                if (idxWord != null && !map.containsKey(idxWord.getLemma())) {
+                    validUnseenWords.add(idxWord);
                     count++;
                 }
                 //TODO: Don't count multiple occurrences of the same word
-                //TODO: Don't count accidental function words, e.g. 'OR' (for Oregon) or 'At' for Astatine
+                //TODO: Don't count accidental function words, e.g. 'OR' (for Oregon) or 'At' (for Astatine)
                 //TODO: Don't count self
-            }
+            //}
         }
-        System.out.println("Returned " + count + " valid senses for words in \"" + gloss + "\"");
-        return validWords;
+        System.out.println("Returned " + count + " valid unseen senses for words in - "
+                + word.getLemma() + ": \"" + word.getSynset().getGloss().split("\"")[0] + "\"");
+        return validUnseenWords;
     }
 }
