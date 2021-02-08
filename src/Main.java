3aa14e3f5155181770fb7e0988a8b4433bda9a89
Eric Gordon <eric.gordon@ucdconnect.ie>
2021-02-08 23:59:01 +0000

    Able to compare sets of words produced by different starting words

--- src/Main.java
+++ src/Main.java
@@ -12,43 +12,61 @@ import java.util.*;
 public class Main {
 
     private static IDictionary dict;
-    private static Map<IIndexWord, IWord> expandedAndPendingWords = new HashMap<>();
+    private static Map<IIndexWord, IWord> expandedAndPendingWords;
 
 
     public static void main(String[] args) throws IOException {
 
         dict = new Dictionary(new File("C:\\Program Files (x86)\\WordNet\\2.1\\dict"));
         dict.open();
+        ArrayList<Map> result = new ArrayList<>();
 
-        Map<IIndexWord, String> expandedWords = new HashMap<>();
+        for(int j = 0; j < 2; j++){
+            Map<IIndexWord, String> expandedWords = new HashMap<>();
+            expandedAndPendingWords = new HashMap<>();
+            Scanner in = new Scanner(System.in);
 
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Starting Word?");
-        String startingWord = in.nextLine();
-        System.out.println("Parts of Speech to include? ('nvar' for all)");  // n for noun, v for verb,
-        String pos = in.nextLine();                                          // a for adjective, and r for adverb
-        LinkedList<IIndexWord> wordList = new LinkedList<>();
-        for (POS p : POS.values()) {    // starting word deliberately expands all parts of speech to begin with,
-            IIndexWord idxWord = dict.getIndexWord(startingWord, p);   // to reduce chance of instantly petering out
-            if (idxWord != null ) {
-                wordList.add(idxWord);
+            System.out.println("Starting Word?");
+            String startingWord = in.nextLine();
+            System.out.println("Parts of Speech to include? ('nvar' for all)");  // n for noun, v for verb,
+            String pos = in.nextLine();                                          // a for adjective, and r for adverb
+            LinkedList<IIndexWord> wordList = new LinkedList<>();
+            for (POS p : POS.values()) {    // starting word deliberately expands all parts of speech to begin with,
+                IIndexWord idxWord = dict.getIndexWord(startingWord, p);   // to reduce chance of instantly petering out
+                if (idxWord != null ) {
+                    wordList.add(idxWord);
+                }
             }
-        }
 
-        int i = 0;
-        while (!wordList.isEmpty() /*&& i < 300*/){
-            IIndexWord idxWord = wordList.remove();
-            expandedWords.put(idxWord, dict.getWord(idxWord.getWordIDs().get(0)).getSynset().getGloss() + "\n");
-            IWordID wordID = idxWord
-                    .getWordIDs()
-                    .get(0);
-            IWord word = dict.getWord(wordID);
-            //System.out.println("Id = " + wordID);
-            System.out.println("-" + word.getLemma());
-            wordList.addAll(expandDefinitionOfWord(word, pos));
-            System.out.println("Iter " + i++ + ": \n\tMap size = " + expandedWords.size() + "\n\tList size = " + wordList.size());
+            int i = 0;
+            while (!wordList.isEmpty() /*&& i < 300*/){
+                IIndexWord idxWord = wordList.remove();
+                expandedWords.put(idxWord, dict.getWord(idxWord.getWordIDs().get(0)).getSynset().getGloss() + "\n");
+                IWordID wordID = idxWord
+                        .getWordIDs()
+                        .get(0);
+                IWord word = dict.getWord(wordID);
+                //System.out.println("Id = " + wordID);
+                System.out.println("-" + word.getLemma());
+                wordList.addAll(expandDefinitionOfWord(word, pos));
+                System.out.println("Iter " + i++ + ": \n\tMap size = " + expandedWords.size() + "\n\tList size = " + wordList.size());
+            }
+            result.add(expandedWords);
         }
+        Map<IIndexWord, String> first = result.get(0);
+        Map<IIndexWord, String> second = result.get(1);
+//        Map<IIndexWord, String> third = result.get(2);
+        HashSet<IIndexWord> diff1and2 = new HashSet<IIndexWord>(first.keySet());
+        diff1and2.removeAll(new HashSet<>(second.keySet()));
+        HashSet<IIndexWord> diff2and1 = new HashSet<IIndexWord>(second.keySet());
+        diff2and1.removeAll(new HashSet<>(first.keySet()));
+        System.out.println("First size: " + result.get(0).size());
+        System.out.println("Second size: " + result.get(1).size());
+//        System.out.println("Third size: " + result.get(2).size());
+        System.out.println("In 1 but not 2: " + diff1and2);
+        System.out.println("In 2 but not 1: " + diff2and1);
+ //        System.out.println("Difference between 2 and 3: ");
+ //       System.out.println("Difference between 1 and 3: ");
     }
     private static List<String> definitionToList(String s){
         String punctuation = "[!._,'@?; ]";
